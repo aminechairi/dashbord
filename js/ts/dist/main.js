2deb8cb1824131d7ae2c1aa8ce53ada7a0c8bedb
amine <aminechairi2000@gmail.com>
2022-10-13 20:21:14 +0300

    finish section settings

--- js/ts/dist/main.js
+++ js/ts/dist/main.js
@@ -19,4 +19,46 @@ for (let i = 0; i < menu.length; i++) {
         title.innerHTML = el.children[0].children[1].innerHTML;
     });
 }
+for (let i = 0; i < document.querySelectorAll(".btn_active .i").length; i++) {
+    let btn_active = {
+        btn_active: document.querySelectorAll(".btn_active")[i],
+        btn_active_i: document.querySelectorAll(".btn_active .i")[i],
+        status: true,
+    };
+    btn_active.btn_active.addEventListener("click", () => {
+        if (btn_active.status == true) {
+            btn_active.btn_active_i.style.right = "38px";
+            btn_active.btn_active_i.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+            btn_active.status = false;
+            btn_active.btn_active.classList.add("inactive");
+        }
+        else {
+            btn_active.btn_active_i.style.right = "2px";
+            btn_active.btn_active_i.innerHTML = `<i class="fa-solid fa-check"></i>`;
+            btn_active.status = true;
+            btn_active.btn_active.classList.remove("inactive");
+        }
+    });
+}
+let backup_manager = {
+    backup_manager: document.querySelectorAll(".dashbord .content .settings_content .boxs .backup_manager .tab_boxs .box"),
+};
+for (let i = 0; i < backup_manager.backup_manager.length; i++) {
+    backup_manager.backup_manager[i].addEventListener("click", (e) => {
+        for (let i = 0; i < backup_manager.backup_manager.length; i++) {
+            let element = backup_manager.backup_manager[i];
+            element.style.border = "1px solid var(--black)";
+            let ic = element.children[0];
+            ic.style.color = "var(--black)";
+            let p = element.children[1];
+            p.style.color = "var(--black)";
+        }
+        let element = e.currentTarget;
+        element.style.border = "2px solid #0075ff";
+        let ic = element.children[0];
+        ic.style.color = "#0075ff";
+        let p = element.children[1];
+        p.style.color = "#0075ff";
+    });
+}
 //# sourceMappingURL=main.js.map
--- js/ts/src/main.ts
+++ js/ts/src/main.ts
@@ -1,4 +1,4 @@
-// input search
+// dashbord input search
 let ab_input = document.querySelectorAll('.dashbord .content .nav_page .input .ab_input')[0] as HTMLDivElement;
 let input = document.querySelectorAll('.dashbord .content .nav_page .input .ab_input input')[0] as HTMLInputElement;
 input.addEventListener('focus', () => {
@@ -20,5 +20,55 @@ for (let i = 0; i < menu.length; i++) {
     let el = e.currentTarget as HTMLElement;
     el.children[0].classList.add('active');
     title.innerHTML = el.children[0].children[1].innerHTML;
+  })
+}
+
+// btn active script
+for (let i = 0; i <  document.querySelectorAll(".btn_active .i").length; i++) {
+  let btn_active:{
+    btn_active: HTMLElement;
+    btn_active_i: HTMLElement;
+    status: boolean;
+  } = {
+    btn_active: document.querySelectorAll(".btn_active")[i] as HTMLMapElement,
+    btn_active_i: document.querySelectorAll(".btn_active .i")[i] as HTMLMapElement,
+    status: true,
+  }
+  btn_active.btn_active.addEventListener("click", () => {
+    if (btn_active.status == true) {
+      btn_active.btn_active_i.style.right = "38px";
+      btn_active.btn_active_i.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+      btn_active.status = false;
+      btn_active.btn_active.classList.add("inactive");
+    } else {
+      btn_active.btn_active_i.style.right = "2px";
+      btn_active.btn_active_i.innerHTML = `<i class="fa-solid fa-check"></i>`;
+      btn_active.status = true;
+      btn_active.btn_active.classList.remove("inactive");
+    }
+  })
+}
+
+// Ssettings 
+// backup manager
+let backup_manager = {
+  backup_manager: document.querySelectorAll(".dashbord .content .settings_content .boxs .backup_manager .tab_boxs .box"),
+}
+for (let i = 0; i < backup_manager.backup_manager.length; i++) {
+  backup_manager.backup_manager[i].addEventListener("click", (e) => {
+    for (let i = 0; i < backup_manager.backup_manager.length; i++) {
+      let element = backup_manager.backup_manager[i] as HTMLElement;
+      element.style.border = "1px solid var(--black)";
+      let ic = element.children[0] as HTMLElement;
+      ic.style.color = "var(--black)";
+      let p = element.children[1] as HTMLElement;
+      p.style.color = "var(--black)";
+    }
+    let element = e.currentTarget as HTMLElement;
+    element.style.border = "2px solid #0075ff";
+    let ic = element.children[0] as HTMLElement;
+    ic.style.color = "#0075ff";
+    let p = element.children[1] as HTMLElement;
+    p.style.color = "#0075ff";
   })
 }
